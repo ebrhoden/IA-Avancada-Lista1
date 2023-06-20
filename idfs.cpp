92dba43ce9705061be2709dd510057a36ddd865c
ebrhoden <edurhoden@gmail.com>
2023-06-20 18:23:09 -0300

    Rollback to original working version

--- idfs.cpp
+++ idfs.cpp
@@ -10,14 +10,10 @@ bool depth_limited_search(Puzzle& current_puzzle, int depth_limit, int& number_e
         vector<State> neighbor_states = current_puzzle.get_neighbor_states();
 
         for(State neighbor_state: neighbor_states){
-            if(neighbor_state.is_goal()){
+            Puzzle child(neighbor_state, &current_puzzle, current_puzzle.depth + 1);
+            bool is_done = depth_limited_search(child, depth_limit - 1, number_expanded_nodes);
+            if(is_done){
                 return true;
-            } else {
-                Puzzle child(neighbor_state, &current_puzzle, current_puzzle.depth + 1);
-                bool is_done = depth_limited_search(child, depth_limit - 1, number_expanded_nodes);
-                if(is_done){
-                    return true;
-                }
             }
         }
     }
@@ -47,12 +43,12 @@ Solution solve_idfs(vector<int> user_input){
 
 Comments:
 
-In this updated code, we perform a depth-limited search for each depth limit starting from 0 
-and increasing until the maximum integer value. If a solution is found at any depth limit, we 
-return the solution. This approach ensures that we explore the search space in a depth-first 
+In this updated code, we perform a depth-limited search for each depth limit starting from 0
+and increasing until the maximum integer value. If a solution is found at any depth limit, we
+return the solution. This approach ensures that we explore the search space in a depth-first
 manner while still guaranteeing completeness.
 
-By using IDDFS, we can avoid the exponential time complexity of traditional depth-first search 
+By using IDDFS, we can avoid the exponential time complexity of traditional depth-first search
 and improve the performance of the IDFS algorithm.
 
 */
