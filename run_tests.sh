2a341719574fc3ea7ec03bb25f9781b34273770d
rodrigomalossi <malossirodrigo@gmail.com>
2023-07-03 23:48:44 -0300

    Merge branch 'main' of https://github.com/ebrhoden/IA-Avancada-Lista1

--- run_tests.sh
+++ run_tests.sh
@@ -9,7 +9,7 @@ do
   # Run the main program with -astar argument and the line as input
   # Limit the execution time to 30 seconds
   # Append the output to 15puzzle_instances.csv
-  if ! gtimeout 30s ./main -astar $line >> 15puzzle_instances.csv
+  if ! gtimeout 300s ./main -astar $line >> 15puzzle_instances.csv
   then
     # If the command times out, append -,-,-,-,- to the csv file
     echo "-,-,-,-,-" >> 15puzzle_instances.csv
