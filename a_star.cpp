44dda89118e04409914046b3b49fa24f94bf1bbd
ebrhoden <edurhoden@gmail.com>
2023-07-13 17:03:00 -0300

    Create vector to do memory clean up

--- a_star.cpp
+++ a_star.cpp
@@ -13,6 +13,7 @@ Solution solve_a_star(vector<int> user_input, HeuristicCalculator h){
     int num_expanded_nodes = 0;
     priority_queue<Puzzle*, vector<Puzzle*>, AStarComparator>* open = new priority_queue<Puzzle*, vector<Puzzle*>, AStarComparator>();
     unordered_set<unsigned long long>* closed = new unordered_set<unsigned long long>();
+    vector<Puzzle*>* clean_up_helper = new vector<Puzzle*>;
     int initial_heuristic_value;
     clock_t start_time = clock();
 
@@ -20,6 +21,7 @@ Solution solve_a_star(vector<int> user_input, HeuristicCalculator h){
     Puzzle* initial_puzzle_pointer = new Puzzle(s, h);
     initial_heuristic_value = initial_puzzle_pointer->heuristic_value;
     open->push(initial_puzzle_pointer);
+    clean_up_helper->push_back(initial_puzzle_pointer);
 
     while(!open->empty()){
         Puzzle* current_puzzle = open->top();
@@ -28,6 +30,13 @@ Solution solve_a_star(vector<int> user_input, HeuristicCalculator h){
         if(closed->find(current_puzzle->state.internal_representation) == closed->end()){
             closed->insert(current_puzzle->state.internal_representation);
             if(current_puzzle->state.is_goal()){
+
+                // Deleting all the allocated memory
+                for (auto p : *clean_up_helper){
+                    delete p;
+                }
+                clean_up_helper->clear();
+
                 return Solution(num_expanded_nodes, current_puzzle->depth, start_time, h.average_result(), initial_heuristic_value);
             }
 
@@ -36,7 +45,9 @@ Solution solve_a_star(vector<int> user_input, HeuristicCalculator h){
 
             for(State neighbor_state: neighbor_states){
                 puzzle_counter++;
-                open->push(new Puzzle(neighbor_state, current_puzzle, current_puzzle->depth + 1, h, puzzle_counter));
+                Puzzle* neighbor_puzzle = new Puzzle(neighbor_state, current_puzzle, current_puzzle->depth + 1, h, puzzle_counter);
+                open->push(neighbor_puzzle);
+                clean_up_helper->push_back(neighbor_puzzle);
             }
         }
     }
